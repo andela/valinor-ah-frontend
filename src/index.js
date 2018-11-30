5ea6b1da9a262d082cbd0eaab0777eb0ad802612
Tersoo Atsen <tersoo.atsen@outlook.com>
2018-11-30 09:47:40 +0100

    chore(sass): setup sass processing
    - install packages
    - update webpack config
    
    [#161290960]

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.scss';
 
 class Index extends Component {
   render() {
