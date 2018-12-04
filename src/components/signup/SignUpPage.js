ceb8385fd782942bafcba002c3550c6d24d22c67
Augustine Ezinwa <augustineezinwa@gmail.com>
2018-12-04 17:37:07 +0100

    feat(login-view): implement login-page-view
    - add login component
    - add login styles
    
    [Finishes #162380276]

--- src/components/signup/SignUpPage.js
+++ src/components/signup/SignUpPage.js
@@ -1,10 +1,8 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../common/Header';
 import emailIcon from '../../../public/assets/icons/mail-icon.svg';
 import userIcon from '../../../public/assets/icons/user-icon.svg';
-import facebookIcon from '../../../public/assets/icons/facebook-icon.svg';
-import googleIcon from '../../../public/assets/icons/google-icon.svg';
-import twitterIcon from '../../../public/assets/icons/twitter-icon.svg';
 
 class SignUp extends React.Component {
   constructor(props) {
@@ -17,64 +15,79 @@ class SignUp extends React.Component {
     return (
       <Fragment>
         <Header />
-        <div className="site-content">
-          <div className="container section">
-            <div className="row justify-content-md-center">
-              <div className="col-sm-12 col-md-4 blue-section">
-                <div className="header">
-                  <div className="box-intro">
-                    <h1 id="welcome">Welcome to Author&apos;s Haven!</h1>
-                    <div className="intro">
-                      <p><span className="text-intro">Enter your personal details to start sharing your exciting ideas.</span></p>
-                    </div>
+        <div className="site-content vertical-center">
+          <div className="container">
+            <div className="row login-wrapper">
+              <div className="col-md-4 signup signup-border text-center ">
+                <div className="row welcome-banner align-items-center">
+                  <div className="col-md-12">
+                    <h2 className="welcome-text">Welcome to Author&apos;s Haven!</h2>
+                    <p className="text-center welcome-body">Enter your personal details to start sharing your exciting ideas.</p>
                   </div>
                 </div>
 
-                <div className="body">
-                  <span className="text-account">Already have an account?</span>
-                  <button type="button" className="btn btn-outline-primary btn-rounded">LOG IN</button>
+                <div className="row login-banner">
+                  <div className="col-md-12">
+                    <p className="account-question">Already have an account?</p>
+
+                    <Link to="/login" className="btn btn-outline-primary signup-btn px-5">LOG IN</Link>
+
+                  </div>
                 </div>
               </div>
-              <div className="col-sm-12 col-md-8 pale-section">
-                <span className="text-create">Create account with your social media</span>
-                <div className="social-button-group">
-                  <button type="button" className="btn btn-google">
-                    <div className="social-icon">
-                      <img src={googleIcon} alt="google icon" />
-                    </div>
-                    <div className="social-text">Login with google</div>
-                  </button>
-                  <button type="button" className="btn btn-facebook">
-                    <div className="social-icon">
-                      <img src={facebookIcon} alt="facebook icon" />
-                    </div>
-                    <div className="social-text">Login with facebook</div>
-                  </button>
-                  <button type="button" className="btn btn-twitter">
-                    <div className="social-icon">
-                      <img src={twitterIcon} alt="twitter icon" />
-                    </div>
-                    <div className="social-text">Login with twitter</div>
-                  </button>
+
+              <div className="col-md-8 login-form login-form-border text-center">
+                <h4 className="sign-in-text">Create account with your social media</h4>
+                {/* Button Div */}
+                <div className="row social-buttons-row">
+
+                  <div className="col-lg-4 social-button">
+                    <button type="button" className="btn btn-primary w-100 google-btn">
+                      <i className="fab fa-google mr-2" />
+                      Signup with Google
+                    </button>
+                  </div>
+
+                  <div className="col-lg-4 social-button">
+                    <button type="button" className="btn btn-primary w-100  facebook-btn">
+                      <i className="fab fa-facebook-f mr-2" />
+                      Signup with Facebook
+                    </button>
+                  </div>
+
+                  <div className="col-lg-4 social-button">
+                    <button type="button" className="btn btn-primary w-100 twitter-btn">
+                      <i className="fab fa-twitter mr-2" />
+                      Signup with Twitter
+                    </button>
+                  </div>
                 </div>
-                <div className="signup-form">
-                  <form className="form-signup">
-                    <span className="text-sign-intro">sign up with your email</span>
-                    <div className="input-group mb-3">
-                      <div className="input-group-prepend">
-                        <span className="input-group-text transparent email-addon" id="basic-addon1"><img src={userIcon} alt=" email icon" /></span>
+                {/* End of button div */}
+
+                {/* user input div */}
+                <h5>Sign up with your email</h5>
+                <form>
+                  <div className="row login-form-section text-center">
+                    <div className="col-md-8 offset-md-2">
+                      <div className="input-group signup-inputs">
+                        <div className="input-group-prepend">
+                          <span className="input-group-text transparent email-addon " id="basic-addon1"><img src={userIcon} alt="email icon" /></span>
+                        </div>
+                        <input type="text" className="form-control email-input" placeholder="FULL NAME" aria-label="Username" aria-describedby="basic-addon1" required />
                       </div>
-                      <input type="text" className="form-control email-input transparent" placeholder="FULL NAME" aria-label="Username" aria-describedby="basic-addon1" />
-                    </div>
-                    <div className="input-group mb-3">
-                      <div className="input-group-prepend">
-                        <span className="input-group-text transparent email-addon" id="basic-addon1"><img src={emailIcon} alt=" email icon" /></span>
+
+                      <div className="input-group signup-inputs">
+                        <div className="input-group-prepend">
+                          <span className="input-group-text transparent email-addon" id="basic-addon1"><img src={emailIcon} alt="email icon" /></span>
+                        </div>
+                        <input type="email" className="form-control email-input" placeholder="EMAIL ADDRESS" aria-label="Username" aria-describedby="basic-addon1" required />
                       </div>
-                      <input type="email" className="form-control email-input transparent" placeholder="EMAIL ADDRESS" aria-label="Username" aria-describedby="basic-addon1" />
+                      <button type="submit" className="btn btn-outline-primary login-btn px-5">SIGN UP</button>
                     </div>
-                    <button type="button" className="btn btn-outline-primary btn-rounded-blue">SIGN UP</button>
-                  </form>
-                </div>
+                  </div>
+                </form>
+                {/* End of user input */}
+
               </div>
             </div>
           </div>
