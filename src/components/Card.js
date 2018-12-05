4cdd39d01fb99c7a46cc6c1df4ef5edaad0a666f
KvNGCzA <akanmuoluwadamilare@yahoo.com>
2018-12-05 21:37:52 +0100

    feature(HomePage): build landing Page
    - build landing page
    - consume fake data
    - style components
    - write tests
    
    [Finishes #162380199]

---
+++ src/components/Card.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export const Card = (props) => {
+  const {
+    title,
+    category,
+    body,
+    backgroundImage
+  } = props;
+
+  return (
+    <div className="card" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="card-overlay">
+        <div className="article-meta">
+          <p className="category-name">{category}</p>
+          <p className="title">{title}</p>
+          <p className="excerpt">{`${body.slice(0, 70)}....`}</p>
+        </div>
+        <button className="btn" type="button">Read More</button>
+      </div>
+    </div>
+  );
+};
+
+export const CardList = (props) => {
+  const { article, title, className } = props;
+  let button;
+  if (className === 'home-articles-cont') {
+    button = (
+      <div className="read-more-btn">
+        <NavLink to="/login" className="category-link">
+          <h1>
+            <span className="title">view more </span>
+            <i className="fas fa-long-arrow-alt-right" />
+          </h1>
+        </NavLink>
+      </div>
+    );
+  }
+  return (
+    <div className={className}>
+
+      <h1 className="article-title">{title}</h1>
+      <NavLink to="/login">
+        {
+          article
+            .map((currentArticle, index) => (
+              <Card
+              category={currentArticle.category}
+              title={currentArticle.title}
+              body={currentArticle.body}
+              backgroundImage={currentArticle.articleImage}
+              key={`card${String(index)}`}
+              />
+            ))
+        }
+      </NavLink>
+
+      {button}
+
+    </div>
+  );
+};
+
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  backgroundImage: PropTypes.string.isRequired
+};
+
+CardList.propTypes = {
+  article: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+};
--- tests/src/components/home/HomePage.test.js
+++ tests/src/components/home/HomePage.test.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import mockData from '../../../../mockdata/articles';
 
-import HomePage from '../../../../src/components/home/HomePage';
+import {
+  PopularPosts,
+  HomePage
+} from '../../../../src/components/home/HomePage';
+
+const { articles } = mockData;
+
+test('PopularPosts snapshot test', () => {
+  const component = shallow(<PopularPosts article={articles} />);
+  expect(component).toMatchSnapshot();
+});
 
 test('Home snapshot test', () => {
   const component = shallow(<HomePage />);
